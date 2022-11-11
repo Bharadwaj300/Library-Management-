b717784fbecccd1ca63e328186afb147820af914
Bharadwaj A.Bharadwaj <adigoppulabharadwaj@gmail.com>
2022-11-11 08:51:53 +0530

    Add styles

--- home.js
+++ home.js
@@ -1,52 +1,52 @@
 //middle of night
-
+var value = 6;
 var books = [{
     "name": "1984",
     "author": "George Orwell",
     "subject": "1984 tells the futuristic story of a dystopian, totalitarian world where free will and love are forbidden.",
     "published date": "08 june 1949",
-    "genre":"sci-fi",
-   "type":"novel",
+    "genre": "sci-fi",
+    "type": "novel",
 },
 {
     "name": "The Lord of the Rings ",
     "author": "J.R.R. Tolkien",
     "subject": "Tolkien’s fantasy epic is one of the top must-read books out there. Set in Middle Earth – a world full of hobbits, elves, orcs, goblins, and wizards ",
     "published date": "29 July 1954",
-    "genre":"fantasy",
-    "type":"novel",
+    "genre": "fantasy",
+    "type": "novel",
 },
 {
     "name": "The Hobbit",
     "author": "J.R.R. Tolkien",
     "subject": "The Hobbit, or There and Back Again is a children's fantasy novel by English author J. R. R. Tolkien.",
     "published date": "21 sep 1937 ",
-    "genre":"fantasy",
-    "type":"novel",
+    "genre": "fantasy",
+    "type": "novel",
 },
 {
     "name": "The Return of the King ",
     "author": "J.R.R. Tolkien",
     "subject": "The story begins in the kingdom of Gondor, which is soon to be attacked by the Dark Lord Sauron.",
     "published date": "20 October 1955",
-    "genre":"fantasy",
-    "type":"novel",
+    "genre": "fantasy",
+    "type": "novel",
 },
 {
     "name": "The Kite Runner ",
     "author": "Khaled Hosseini",
     "subject": "The Kite Runner is a moving story of an unlikely friendship between a wealthy boy and the son of his father’s servant.",
     "published date": "29 May 2003",
-    "genre":"fiction",
-    "type":"novel",
+    "genre": "fiction",
+    "type": "novel",
 },
 {
     "name": "A Thousand Splendid Suns",
     "author": "Khaled Hosseini",
     "subject": "A Thousand Splendid Suns is a 2007 novel by Afghan-American author Khaled Hosseini, following the huge success of his bestselling 2003 debut The Kite Runner. ",
     "published date": "22 May 2007",
-    "genre":"fiction",
-    "type":"novel",
+    "genre": "fiction",
+    "type": "novel",
 
 },
 {
@@ -54,17 +54,17 @@ var books = [{
     "author": "Khaled Hosseini",
     "subject": "And the Mountains Echoed is the third novel by Afghan-American author Khaled Hosseini. It deviates from Hosseini's style in his first two works through his choice to avoid focusing on any one character.",
     "published date": "21 May 2013",
-    "genre":"fiction",
-    "type":"novel",
+    "genre": "fiction",
+    "type": "novel",
 },
 {
     "name": "Harry Potter and the Philosopher’s Stone ",
     "author": "J.K. Rowling",
     "subject": "Love conquers hate. The most significant of Harry's advantages was love. Harry survived Voldemort's curse because of his mother's instinct to protect him. Love literally saved Harry's life, and its power remained as the magic associated with his mother's sacrifice took root in the home of his only surviving relative.",
     "published date": "26 June 1997",
-    "genre":"fantasy",
-    "type":"novel",
-    
+    "genre": "fantasy",
+    "type": "novel",
+
 
 },
 {
@@ -72,16 +72,16 @@ var books = [{
     "author": "J.K. Rowling",
     "subject": "“Harry Potter and the Cursed Child” is about the journey Albus takes while growing up, and the roles he and his best friend, Scorpius (Draco Malfoy's son), play when dark forces, perhaps in league with Voldemort, once again threaten the fate of the planet.",
     "published date": "30 July 2016",
-    "genre":"fantasy",
-    "type":"novel",
+    "genre": "fantasy",
+    "type": "novel",
 },
 {
     "name": "The Solid Life of Sugar Water",
     "author": " Jack Thorne",
     "subject": "Jack Thorne's superb drama of love and loss.",
     "published date": "2015",
-    "genre":"romance",
-    "type":"novel",
+    "genre": "romance",
+    "type": "novel",
 },
 {
 
@@ -89,32 +89,32 @@ var books = [{
     "author": "Jack Thorne",
     "subject": "Jack Thorne's play When You Cure Me is a bittersweet and poignant tale of love and misunderstanding – and discovering that what you say and do can be very different from what you think and feel.",
     "published date": "2005",
-    "genre":"romance",
-    "type":"novel",
+    "genre": "romance",
+    "type": "novel",
 },
 {
     "name": "Slaughterhouse-Five",
     "author": "Kurt Vonnegut",
     "subject": "A Duty-Dance with Death is a 1969 semi-autobiographic science fiction-infused anti-war novel",
     "published date": "31 March 1969",
-    "genre":"fiction",
-    "type":"novel",
+    "genre": "fiction",
+    "type": "novel",
 },
 {
     "name": "WIRELESS COMMUNICATIONS",
     "author": "Andreas F.Molisch",
     "subject": "Advanced undergraduate students (with a working knowledge of standard digital communications) · Graduate students and practising engineers who want to learn the subject· ",
     "published date": "2007",
-    "genre":"education",
-    "type":"education",
+    "genre": "education",
+    "type": "education",
 },
 {
     "name": "The Lion, the Witch, and the Wardrobe",
     "author": "C. S. Lewis",
     "subject": "fantasy novel for children",
     "published date": "16 October 1950",
-    "genre":"fantasy",
-    "type":"novel",
+    "genre": "fantasy",
+    "type": "novel",
 },
 {
 
@@ -122,129 +122,128 @@ var books = [{
     "author": "C. S. Lewis",
     "subject": "fantasy novel for children",
     "published date": "6 September 1954",
-    "genre":"fantasy",
-    "type":"novel",
+    "genre": "fantasy",
+    "type": "novel",
 },
 {
-    "name":"Dune",
-    "author":"Frank Herbert",
-    "subject":"Dune focuses on Paul Atreides, the 15-year-old heir to House Atreides. His family is forced by the Emperor who rules the Known Universe to leave the planet Caladan and to take control of the planet Arrakis. ",
-    "published date":"1965", 
-    "genre":"sci-fi",
-    "type":"novel",
+    "name": "Dune",
+    "author": "Frank Herbert",
+    "subject": "Dune focuses on Paul Atreides, the 15-year-old heir to House Atreides. His family is forced by the Emperor who rules the Known Universe to leave the planet Caladan and to take control of the planet Arrakis. ",
+    "published date": "1965",
+    "genre": "sci-fi",
+    "type": "novel",
 
 },
 {
-    "name":"Neuromancer",
-    "author":"William Gibson",
-    "subject":"Neuromancer is a novel deeply entrenched in themes of individual identity and relevance of physical human embodiment.",
-    "published date":"1984", 
-    "genre":"sci-fi",
-    "type":"novel",
+    "name": "Neuromancer",
+    "author": "William Gibson",
+    "subject": "Neuromancer is a novel deeply entrenched in themes of individual identity and relevance of physical human embodiment.",
+    "published date": "1984",
+    "genre": "sci-fi",
+    "type": "novel",
 
 },
 {
-    "name":"The Three-Body Problem",
-    "author":"Liu Cixin",
-    "subject":"The title refers to the three-body problem in orbital mechanics. It is the first novel of the Remembrance of Earth's Past trilogy",
-    "published date":"2008", 
-    "genre":"sci-fi",
-    "type":"novel",
+    "name": "The Three-Body Problem",
+    "author": "Liu Cixin",
+    "subject": "The title refers to the three-body problem in orbital mechanics. It is the first novel of the Remembrance of Earth's Past trilogy",
+    "published date": "2008",
+    "genre": "sci-fi",
+    "type": "novel",
 
 },
 {
-    "name":"Project hail Mary",
-    "author":"Andy Weir",
-    "subject":"Set in the near future, the novel centers on junior high (middle) school-teacher-turned-astronaut Ryland Grace, who wakes up from a coma afflicted with amnesia.",
-    "published date":"2021", 
-    "genre":"sci-fi",
-    "type":"novel",
+    "name": "Project hail Mary",
+    "author": "Andy Weir",
+    "subject": "Set in the near future, the novel centers on junior high (middle) school-teacher-turned-astronaut Ryland Grace, who wakes up from a coma afflicted with amnesia.",
+    "published date": "2021",
+    "genre": "sci-fi",
+    "type": "novel",
 
 },
 {
-    "name":"Frankenstein",
-    "author":"Mary Shelley",
-    "subject":"Frankenstein tells the story of Victor Frankenstein, a young scientist who creates a sapient creature in an unorthodox scientific experiment.",
-    "published date":"1818", 
-    "genre":"sci-fi",
-    "type":"novel",
+    "name": "Frankenstein",
+    "author": "Mary Shelley",
+    "subject": "Frankenstein tells the story of Victor Frankenstein, a young scientist who creates a sapient creature in an unorthodox scientific experiment.",
+    "published date": "1818",
+    "genre": "sci-fi",
+    "type": "novel",
 
 },
 {
-    "name":"The Luminous Dead",
-    "author":"Caitlin Starling",
-    "subject":"A thrilling, atmospheric debut with the intensive drive of The Martian and Gravity and the creeping dread of Annihilation, in which a caver on a foreign planet finds herself on a terrifying psychological and emotional journey for survival.",
-    "published date":"2019", 
-    "genre":"Horror",
-    "type":"novel",
+    "name": "The Luminous Dead",
+    "author": "Caitlin Starling",
+    "subject": "A thrilling, atmospheric debut with the intensive drive of The Martian and Gravity and the creeping dread of Annihilation, in which a caver on a foreign planet finds herself on a terrifying psychological and emotional journey for survival.",
+    "published date": "2019",
+    "genre": "horror",
+    "type": "novel",
 
 },
 {
-    "name":"The Death of Jane Lawrence",
-    "author":"Caitlin Starling ",
-    "subject":"The Death of Jane Lawrence digs into the age-old question asked by gothic novels and fairy tales alike: What do you do if your deeply attractive but hastily-married husband is hiding a very dark secret in his crumbling manor ",
-    "published date":"2021", 
-    "genre":"Horror",
-    "type":"novel",
+    "name": "The Death of Jane Lawrence",
+    "author": "Caitlin Starling ",
+    "subject": "The Death of Jane Lawrence digs into the age-old question asked by gothic novels and fairy tales alike: What do you do if your deeply attractive but hastily-married husband is hiding a very dark secret in his crumbling manor ",
+    "published date": "2021",
+    "genre": "horror",
+    "type": "novel",
 
 },
 {
-    "name":"Steve Jobs",
-    "author":"Walter Isaacson",
-    "subject":"Steve Jobs is the authorized self-titled biography of American business magnate and Apple co-founder Steve Jobs.",
-    "published date":"2011", 
-    "genre":"biography",
-    "type":"novel",
+    "name": "Steve Jobs",
+    "author": "Walter Isaacson",
+    "subject": "Steve Jobs is the authorized self-titled biography of American business magnate and Apple co-founder Steve Jobs.",
+    "published date": "2011",
+    "genre": "biography",
+    "type": "novel",
 
 },
 {
-    "name":"Wings of Fire",
-    "author":"A. P. J. Abdul Kalam",
-    "subject":"Wings of Fire (1999), is the autobiography of the Missile Man of India and President of India, Dr. A. P. J. Abdul Kalam. It was written by him and Arun Tiwari.",
-    "published date":"1999",
-    "genre":"biography",
-    "type":"novel",
+    "name": "Wings of Fire",
+    "author": "A. P. J. Abdul Kalam",
+    "subject": "Wings of Fire (1999), is the autobiography of the Missile Man of India and President of India, Dr. A. P. J. Abdul Kalam. It was written by him and Arun Tiwari.",
+    "published date": "1999",
+    "genre": "biography",
+    "type": "novel",
 
 },
 {
-    "name":"The Wedding Date",
-    "author":"Jasmine Guillory",
-    "subject":"A groomsman and his last-minute guest are about to discover if a fake date can go the distance in this fun and flirty multicultural romance",
-    "published date":"2018", 
-    "genre":"romance",
-    "type":"novel",
+    "name": "The Wedding Date",
+    "author": "Jasmine Guillory",
+    "subject": "A groomsman and his last-minute guest are about to discover if a fake date can go the distance in this fun and flirty multicultural romance",
+    "published date": "2018",
+    "genre": "romance",
+    "type": "novel",
 
 },
 {
-    "name":"DA VINCI CODE",
-    "author":"Dan Brown",
-    "subject":"The Da Vinci Code heralds the arrival of a new breed of lightning-paced, intelligent thriller…surprising at every twist, absorbing at every moment.",
-    "published date":"2003", 
-    "genre":"mystery",
-    "type":"novel",
+    "name": "DA VINCI CODE",
+    "author": "Dan Brown",
+    "subject": "The Da Vinci Code heralds the arrival of a new breed of lightning-paced, intelligent thriller…surprising at every twist, absorbing at every moment.",
+    "published date": "2003",
+    "genre": "mystery",
+    "type": "novel",
 
 },
 {
-    "name":"Digital Image Processing",
-    "author":"Rafael C. Gozalez",
-    "subject":"Completely self-contained and heavily illustrated this introduction to basic concepts and methodologies for digital image processing is written at a level that truly is suitable for seniors and first-year graduate",
-    "published date":"2008", 
-    "genre":"education",
-    "type":"education",
+    "name": "Digital Image Processing",
+    "author": "Rafael C. Gozalez",
+    "subject": "Completely self-contained and heavily illustrated this introduction to basic concepts and methodologies for digital image processing is written at a level that truly is suitable for seniors and first-year graduate",
+    "published date": "2008",
+    "genre": "education",
+    "type": "education",
 
 },
 
 ];
 
-
-const showBooks = (books,prompt) => {
+var cards = document.getElementById("cards");
+const showBooks = (books, prompt) => {
 
     var card = document.getElementById("cards");
     card.innerHTML = "";
 
     for (var i = 0; i < books.length; ++i) {
         var childDiv1 = document.createElement("div");
-        childDiv1.innerHTML = "I am here";
 
         childDiv1.id = 'card';
         childDiv1.classList.add("card");
@@ -255,9 +254,9 @@ const showBooks = (books,prompt) => {
         var title = document.createElement("p");
         var subject = document.createElement("p");
         var date = document.createElement("p");
-        var img=document.createElement('img');
+        var img = document.createElement('img');
         img.classList.add("card-img-top");
-        img.src="images/image"+(i+1)+".jpeg";
+        img.src = "images/image" + (i + 1) + ".jpeg";
         //<img class="card-img-top" src="..." alt="Card image cap"></img>
         author.innerHTML = books[i]['author'];
         title.innerHTML = books[i]['name'];
@@ -267,16 +266,25 @@ const showBooks = (books,prompt) => {
         newdiv.appendChild(img);
         newdiv.appendChild(author);
         newdiv.appendChild(title);
-        newdiv.appendChild(subject);
+        //newdiv.appendChild(subject);
         newdiv.appendChild(date);
         childDiv1.appendChild(newdiv);
         card.appendChild(childDiv1);
 
 
     }
-    document.getElementById("error").innerHTML = prompt+ books.length + " results";
+    document.getElementById("error").innerHTML = prompt +" " +( value>27 ? 27:value)+ " results";
 }
-showBooks(books,"Showing ");
+
+window.onload = (event) => {
+
+    var filteredbooks = [];
+    for (var i = 0; i < 6; ++i) {
+        filteredbooks.push(books[i]);
+    }
+    showBooks(filteredbooks, "Showing ");
+};
+
 
 
 var filerbutton = document.getElementById("filter")
@@ -292,11 +300,11 @@ filerbutton.addEventListener('click', (e) => {
     console.log(typemenu.value, authormenu.value, genremenu.value);
     var filteredbooks = [];
     for (var i = 0; i < books.length; ++i) {
-        if (books[i]['author'] == authormenu.value && books[i]['genre'] == genremenu.value && books[i]['type'] == typemenu.value) {
+        if (books[i]['author'] == authormenu.value || books[i]['genre'] == genremenu.value || books[i]['type'] == typemenu.value) {
             filteredbooks.push(books[i]);
         }
     }
-    showBooks(filteredbooks,"Found ");
+    showBooks(filteredbooks, "Found ");
 })
 
 var removefilerbutton = document.getElementById("removefilter")
@@ -304,12 +312,42 @@ var removefilerbutton = document.getElementById("removefilter")
 removefilerbutton.addEventListener('click', (e) => {
     e.preventDefault();
     var filteredbooks = [];
-    for (var i = 0; i < books.length; ++i) {
+    for (var i = 0; i < 6; ++i) {
         filteredbooks.push(books[i]);
     }
 
-    showBooks(filteredbooks,"Showing ");
+    showBooks(filteredbooks, "Showing ");
 })
 
 
+window.onscroll = function (ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        // you're at the bottom of the page
+        if (value <= 27) {
+            const myTimeout = setTimeout(loading, 50);
 
+            function loading() {
+                console.log("Scrolled");
+                var loader = document.createElement("div");
+                loader.id = "loader";
+                loader.classList.add("loader");
+                cards.append(loader);
+                setTimeout(() => {
+                    loader.remove();
+                    value = value + 3;
+                    var filerbooks = []
+                    for (var i = 0; i < value && i < 27; ++i) {
+
+                        filerbooks.push(books[i]);
+
+                    }
+                    showBooks(filerbooks, "Showing");
+                    
+                }, 200)
+
+            }
+
+
+        }
+    }
+};
